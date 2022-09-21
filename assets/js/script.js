bd33b121de0f1f2e8c87e2360462e7bb821fdf93
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-09-21 15:16:09 -0500

    Reworking Js logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,73 +2,95 @@ var start = document.querySelector('#start');
 var timerEl = document.querySelector('#timer');
 var questionCard = document.querySelector('#question');
 var score = 0;
+var qIndex = 0;
 
-//Made a class to quickly add new questions, and keep
-//the user update (display) function stored as a method
 class question {
     constructor(question, correct, answers) {
         this.question = question;
         this.correct = correct;
         this.answers = answers;
     }
-    display() {
-        // Show question
-        var heading = document.createElement('h2');
-        heading.innerText = this.question;
-        questionCard.appendChild(heading);
-
-        // This is to add the correct question to a random part of the answers
-        var correctIndex = Math.floor(Math.random() * this.answers.length + 1);
-        var answerIndex = 0;
-
-        // Add answer to question div
-        for (let i = 0; i < this.answers.length + 1; i++) {
-            if (i === correctIndex) {
-                let currentAnswer = document.createElement('h3');
-                currentAnswer.innerText = this.correct;
-                questionCard.appendChild(currentAnswer);
-            } else {
-                let currentAnswer = document.createElement('p');
-                currentAnswer.innerText = this.answers[answerIndex];
-                questionCard.appendChild(currentAnswer);
-                answerIndex++;
-            }
-        }
-    }
 }
 
 // Questions and Answers
 let q1 = new question('What is the meaning of this?', 'correct', ['ans1', 'ans2']);
 let q2 = new question('What is the meaning of this2?', 'correct', ['2ans1', '2ans2']);
-var queue = [q1, q2];
-qIndex = 0;
-var totalTime = 10 * queue.length;
+var unshuffledQueue = [q1, q2];
+
+// shuffle questions
+var queue = [];
+
+while (unshuffledQueue.length) {
+    var random = Math.floor(Math.random() * unshuffledQueue.length);
+        item = unshuffledQueue.splice(random, 1);
+    queue.push(item[0])
+}
+
+const queueLen = queue.length;
+
+// Add all questions and answers to page
+for (let i = 0; i < queueLen; i++) {
+
+    // add div with id for each question
+    var div = document.createElement('div');
+    div.setAttribute('id', i)
+    questionCard.appendChild(div);
+
+    // add question
+    var q = queue.shift();
+
+    var h2 = document.createElement('h2');
+    h2.innerHTML = q.question;
+    div.appendChild(h2)
+
+    // add answers and correct in randomly
+    var correctIndex = Math.floor(Math.random() * q.answers.length + 1);
+    var answerIndex = 0;
+
+    for (let j = 0; j < q.answers.length + 1; j++) {
+
+        if (j === correctIndex) {
+            h3 = document.createElement('h3');
+            h3.innerHTML = q.correct;
+            questionCard.appendChild(h3);
+        } else {
+            p = document.createElement('p');
+            p.innerHTML = q.answers[answerIndex];
+            questionCard.appendChild(p);
+            answerIndex++;
+        }
+
+    }
+
+}
+
+// timer
+var totalTime = 10 * queueLen;
 timerEl.textContent = totalTime;
 
-// start quiz is clicked
-start.addEventListener('click', () => {
-    
-    // A timer of 10s per question (6 questions ? 60s) begins countdown
+function game() {
 
-    var globalTimer = setInterval( () => {
+    var timer = setInterval( () => {
 
         totalTime--;
         timerEl.textContent = totalTime;
 
-        if (totalTime === 0 || qIndex === queue.length - 1) {
-            clearInterval(globalTimer);
-            questionCard.innerHTML = '';
-            //show score
+        if (totalTime <= 0 || queue.length === 0) {
+            clearInterval(timer);
+            // Allow user to save score and initials
         }
-
+    
     }, 1000);
 
-    queue[qIndex].display();
-    // These vars must be declared while a question is being displayed
-    var correctAnswers = document.querySelectorAll('h3');
-    var wrongAnswers = document.querySelectorAll('p');
-    console.log(correctAnswers, wrongAnswers)
+    // Show question until clicked
 
+
+}
+
+start.addEventListener('click', () => {
+    game();
 });
 
-// TODO: Add event listeners to p and h3 tags to catch user click
+questionCard.addEventListener('click', (e) => {
+    console.log(e.target.nodeName)
+});
